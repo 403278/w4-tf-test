eaf19130c28a5337e7e627b6a523e44ad936bf3e
Gabriel I. Rafael <g.rafael@student.fontys.nl>
2021-03-22 13:32:53 +0100

    Updated resource

--- resource.tf
+++ resource.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0767046d1677be5a0"
   instance_type = "t2.micro"
-  key_name      = "AnsibleCN"
+  key_name      = "SASadminKP-ID"
   security_groups = ["SSH to Ansible Control Node"]
 
   tags = {
